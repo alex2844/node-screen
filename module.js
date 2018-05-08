b381b577e412ad0fb38f8da47b92abf57b8d64fe
Alex Smith <alex.smith2844@gmail.com>
2018-05-08 15:41:49 +0500

    mouse move && click for linux

--- module.js
+++ module.js
@@ -26,24 +26,36 @@ module.exports = {
 			}
 		}
 	},
-	shot: function(filePath, callback) {
+	shot: function(path, callback) {
 		(function(cb) {
 			if (os.platform() !== 'win32')
 				cb();
 			else
 				fs.createReadStream(path.resolve(__dirname, 'bin/nircmdc.exe')).pipe(fs.createWriteStream(process.env['TEMP']+'\\nircmdc.exe').on('finish', cb));
 		})(function() {
-			exec(module.exports.cmd.shot(filePath), function(err, res, stderr) {
+			exec(module.exports.cmd.shot(path), function(err, res, stderr) {
 				if (err && os.platform() !== 'win32')
 					return callback(err.message, null, err);
 				else
 					fs.unlink(process.env['TEMP']+'\\nircmdc.exe', function() {});
-				fs.exists(filePath, function(exists) {
+				fs.exists(path, function(exists) {
 					if (!exists)
 						return callback('Screenshot failed', null, new Error('Screenshot failed'));
-					callback(null, filePath);
+					callback(null, path);
 				});
 			});
 		});
+	},
+	mousemove: function(coords, callback) {
+		console.log(coords);
+		exec('xte "mousemove '+coords.x+' '+coords.y+'"', callback);
+	},
+	mouseclick: function(key, callback) {
+		var code = ['left', 'middle', 'right'].indexOf(key) + 1;
+		console.log({key: key});
+		if (code > 0)
+			exec('xte "mouseclick '+code+'"', callback);
+		else
+			return callback('Detect key failed', null, new Error('Detect key failed'));
 	}
 }
