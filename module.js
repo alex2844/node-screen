0a1339fedd4bcc38a542109dc814f1d63965e770
Alex Smith <alex.smith2844@gmail.com>
2018-05-14 08:45:58 +0500

    'pre-init'

--- module.js
+++ module.js
@@ -3,11 +3,10 @@ var exec = require('child_process').exec,
 	fs = require('fs'),
 	path = require('path');
 
-module.exports = {
-	pkg: [],
+(module.exports = {
 	cmd: {
 		shot: function(output, width) {
-			switch (os.platform()) {
+			switch (process.platform) {
 				case 'win32': {
 					return process.env['TEMP']+'\\shot.exe '+output+(width ? ' '+width : '');
 				}
@@ -26,7 +25,7 @@ module.exports = {
 			}
 		},
 		mousemove: function(x, y) {
-			switch (os.platform()) {
+			switch (process.platform) {
 				case 'win32': {
 					return process.env['TEMP']+'\\mouse.exe mousemove '+x+' '+y;
 				}
@@ -39,7 +38,7 @@ module.exports = {
 			}
 		},
 		mouseclick: function(code) {
-			switch (os.platform()) {
+			switch (process.platform) {
 				case 'win32': {
 					return process.env['TEMP']+'\\mouse.exe mouseclick '+code;
 				}
@@ -52,51 +51,46 @@ module.exports = {
 			}
 		}
 	},
-	init: function(bin, callback) {
-		if ((module.exports.pkg.indexOf(bin) > -1) || (os.platform() !== 'win32'))
-			callback();
-		else{
-			module.exports.pkg.push(bin);
-			fs.createReadStream(path.resolve(__dirname, 'bin/'+bin+'.exe')).pipe(fs.createWriteStream(process.env['TEMP']+'\\'+bin+'.exe').on('finish', callback));
-		}
-	},
 	shot: function(output, width, callback) {
 		if (!callback && width) {
 			callback = width;
 			width = null;
 		}
-		module.exports.init('shot', function() {
-			exec(module.exports.cmd.shot(output, width), function(err, res) {
-				if (typeof(callback) == 'function') {
-					if (err && (os.platform() !== 'win32'))
-						return callback(err.message, null, err);
-					fs.exists(output, function(exists) {
-						if (!exists)
-							return callback('Screenshot failed', null, new Error('Screenshot failed'));
-						callback(null, output);
-					});
-				}
-			});
+		exec(module.exports.cmd.shot(output, width), function(err, res) {
+			if (typeof(callback) == 'function') {
+				if (err && (process.platform != 'win32'))
+					return callback(err.message, null, err);
+				fs.exists(output, function(exists) {
+					if (!exists)
+						return callback('Screenshot failed', null, new Error('Screenshot failed'));
+					callback(null, output);
+				});
+			}
 		});
 	},
 	mousemove: function(coords, callback) {
-		module.exports.init('mouse', function() {
-			exec(module.exports.cmd.mousemove(Math.round(coords.x), Math.round(coords.y)), function(err, res) {
-				if (typeof(callback) == 'function')
-					callback(err, res, err);
-			});
+		exec(module.exports.cmd.mousemove(Math.round(coords.x), Math.round(coords.y)), function(err, res) {
+			if (typeof(callback) == 'function')
+				callback(err, res, err);
 		});
 	},
 	mouseclick: function(key, callback) {
 		var code = ['left', 'middle', 'right'].indexOf(key) + 1;
 		if (code > 0)
-			module.exports.init('mouse', function() {
-				exec(module.exports.cmd.mouseclick(code), function(err, res) {
-					if (typeof(callback) == 'function')
-						callback(err, res, err);
-				});
+			exec(module.exports.cmd.mouseclick(code), function(err, res) {
+				if (typeof(callback) == 'function')
+					callback(err, res, err);
 			});
 		else
 			return callback('Detect key failed', null, new Error('Detect key failed'));
+	},
+	main: function() {
+		console.log('Node-screen main');
+		if (process.platform == 'win32')
+			['shot', 'mouse'].map(function(bin) {
+				fs.createReadStream(path.resolve(__dirname, 'bin/'+bin+'.exe')).pipe(fs.createWriteStream(process.env['TEMP']+'\\'+bin+'.exe').on('finish', function() {
+					console.log('extract bin: '+bin);
+				}));
+			});
 	}
-}
+}).main();
